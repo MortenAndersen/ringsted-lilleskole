d228f90aa1fecdaa969072fe61422a52e0e339fa
Morten Andersen <morten@hjemmesider.dk>
2021-06-09 10:05:56 +0200

    hvid facebook

--- functions.php
+++ functions.php
@@ -9,35 +9,21 @@ add_action( 'wp_enqueue_scripts', 'stc_enqueue_styles' );
 
 // Header style
 function web_header_style() {
-    echo 'wrap a-center';
+    echo 'a-center';
 }
-
-/*
-function web_header_sticky() {
-    echo 'no-sticky-header';
-}
-*/
-
-/*
-function web_menu_pos() {
-	echo 'right-align';
-}
-*/
 
 function ringsted_intra_link() {
     echo '<nav class="intranet-menu">';
-        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Li" target="_blank">PersonaleIntra</a>';
-        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Fi" target="_blank">ForældreIntra</a>';
-        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Pi" target="_blank">ElevIntra</a>';
+        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Li" target="_blank" class="intra">PersonaleIntra</a>';
+        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Fi" target="_blank" class="intra">ForældreIntra</a>';
+        echo '<a href="http://www.ringsted-lilleskole.skoleintra.dk/Pi" target="_blank" class="intra">ElevIntra</a>';
+        echo '<div class="social">';
+        echo '<a href="https://www.facebook.com/ringstedlilleskole" target="_blank"><img src="' . get_stylesheet_directory_uri() . '/img/facebook.svg" alt="Facebook"></a>';
+    echo '</div>';
     echo '</nav>';
 }
 
 // Social Icon
 
 function web_social_menu()
-    {
-    echo '<div class="social">';
-        echo '<a href="https://www.facebook.com/ringstedlilleskole" target="_blank"><img src="' . get_stylesheet_directory_uri() . '/img/facebook.svg" alt="Facebook"></a>';
-    echo '</div>';
-
-    }
+    { }
