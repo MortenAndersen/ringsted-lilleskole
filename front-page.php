e34947dbc0c6de3fb826de7926b2f9f02925f7ec
Morten Andersen <morten@hjemmesider.dk>
2021-06-10 09:58:45 +0200

    forside 2

--- front-page.php
+++ front-page.php
@@ -1,21 +1,34 @@
 <?php
 get_header();
 
-echo '<section class="bg-color-1">';
+echo '<section class="bg-color-xxx">';
 	echo '<div class="wrap grid g-d-3 gap-4 pad-4">';
-		echo '<div class="col-1">';
-			echo 'her er jeg';
+		echo '<div class="col-1">'; ?>
+
+		<div id="text-2" class="widget footer-col widget_text"><h4 class="widget-title widget-title-footer">Kontakt skolen</h4>			<div class="textwidget"><p>Næstvedvej 349 B<br>
+4100 Ringsted<br>
+Tlf: 82304100<br>
+<a href="mailto:Kontor@ringsted-lilleskole.dk">Kontor@ringsted-lilleskole.dk</a></p>
+</div>
+		</div>
+			<?php
+		echo '</div>';
+		echo '<div class="col-1">'; ?>
+<div id="text-3" class="widget footer-col widget_text"><h4 class="widget-title widget-title-footer">Kontakt Oasen</h4>			<div class="textwidget"><p>Tlf. 27 84 05 74<br>
+Morgen 6.30 – 8.00<br>
+Eftermiddag 12.45 – 17.00<br>
+<em>(dog kun til 16.00 om fredagen)</em></p>
+</div>
+		</div>
+<?php
 		echo '</div>';
 		echo '<div class="col-1">';
-			echo 'her er jeg';
-		echo '</div>';
-		echo '<div class="col-1">';
-			echo 'her er jeg';
+			echo '<h3>Nyheder</h3>';
 		echo '</div>';
 	echo '</div>';
-echo '</section>';
+echo '</section>'; ?>
 
-get_template_part('template-parts/content/page', 'article'); ?>
+
 
 
 
@@ -29,13 +42,15 @@ get_template_part('template-parts/content/page', 'article'); ?>
 
 <?php 
 
-echo '<section class="bg-color-2">';
+get_template_part('template-parts/content/page', 'article'); 
+
+echo '<section class="bg-color-1">';
 	echo '<div class="wrap grid g-d-3 gap-4 pad-4">';
 		echo '<div class="col-1">';
-			echo '<h3>Kontakt skolen</h3>';
+			echo 'her er jeg';
 		echo '</div>';
 		echo '<div class="col-1">';
-			echo '<h3>Kontakt Oasen</h3>';
+			echo 'her er jeg';
 		echo '</div>';
 		echo '<div class="col-1">';
 			echo 'her er jeg';
