dcc6aad805d6e00ca449434c0be22824124713c7
Morten Andersen <morten@hjemmesider.dk>
2021-06-11 10:51:19 +0200

    forside flow

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@ get_header();
 
 echo '<section class="bg-color-xxx">';
 	echo '<div class="wrap grid g-d-3 gap-2">';
-		echo '<div class="col-1">'; ?>
+		echo '<div class="column">'; ?>
 
 		<div id="text-2" class="widget footer-col widget_text"><h4 class="widget-title widget-title-footer">Kontakt skolen</h4>			<div class="textwidget"><p>Næstvedvej 349 B<br>
 4100 Ringsted<br>
@@ -13,7 +13,7 @@ Tlf: 82304100<br>
 		</div>
 			<?php
 		echo '</div>';
-		echo '<div class="col-1">'; ?>
+		echo '<div class="column">'; ?>
 <div id="text-3" class="widget footer-col widget_text"><h4 class="widget-title widget-title-footer">Kontakt Oasen</h4>			<div class="textwidget"><p>Tlf. 27 84 05 74<br>
 Morgen 6.30 – 8.00<br>
 Eftermiddag 12.45 – 17.00<br>
@@ -22,8 +22,9 @@ Eftermiddag 12.45 – 17.00<br>
 		</div>
 <?php
 		echo '</div>';
-		echo '<div class="col-1">';
+		echo '<div class="column">';
 			echo '<h4 class="widget-title">Nyheder</h4>';
+			echo '<p>Her kan vi vise de sidste 3 nyehder</p>';
 		echo '</div>';
 	echo '</div>';
 echo '</section>'; ?>
@@ -42,20 +43,62 @@ echo '</section>'; ?>
 
 <?php 
 
-get_template_part('template-parts/content/page', 'article'); 
+get_template_part('template-parts/content/page', 'article'); ?>
 
-echo '<section class="bg-color-1">';
-	echo '<div class="wrap grid g-d-3 gap-4 pad-4">';
-		echo '<div class="col-1">';
-			echo 'her er jeg';
-		echo '</div>';
-		echo '<div class="col-1">';
-			echo 'her er jeg';
-		echo '</div>';
-		echo '<div class="col-1">';
-			echo 'her er jeg';
-		echo '</div>';
-	echo '</div>';
-echo '</section>';
+<section style="display:none;" class="bg-color-3">
+	<div class="wrap grid g-d-2 gap-2">
 
-get_footer();
+		<div class="video">
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/Ehk7Fj0pgIg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+
+		<div class="facebook-wall">
+
+		</div>
+	</div>
+</section>
+
+<section class="bg-color-1">
+	<div class="wrap grid g-d-3 gap-4 pad-4">
+		<div class="column">
+
+		<h4 class="widget-title">Ny elev</h4>
+		<img src="https://images.pexels.com/photos/3755619/pexels-photo-3755619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+		<div class="column">
+			<h4 class="widget-title">Stærk faglig integration</h4>
+			<img src="https://images.pexels.com/photos/159823/kids-girl-pencil-drawing-159823.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+		<div class="column">
+			<h4 class="widget-title">Oasen</h4>
+			<img src="https://images.pexels.com/photos/1148998/pexels-photo-1148998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+
+		<div class="column">
+			<h4 class="widget-title">Værdigrundlag</h4>
+			<img src="https://images.pexels.com/photos/3784567/pexels-photo-3784567.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+		<div class="column">
+			<h4 class="widget-title">Undervisningen</h4>
+			<img src="https://images.pexels.com/photos/5212345/pexels-photo-5212345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+		<div class="col">
+			<h4 class="widget-title">Personalet</h4>
+			<img src="https://images.pexels.com/photos/1595387/pexels-photo-1595387.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+			<p>Elementum! Consectetuer est ex accumsan pulvinar, gravida non commodi risus! Mi pretium. Quae orci quo mi. Netus ullam netus mauris magni reprehenderit ac molestie sequi! Ea molestiae quo, aute! Impedit dicta porttitor asperiores, molestie molestias elit excepteur quis habitasse! Conubia.</p>
+			<p class="read-more"><a href="#">Læs mere</a></p>
+		</div>
+	</div>
+</section>
+
+<?php get_footer();
